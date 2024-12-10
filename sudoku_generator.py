1bc241937d3dd0b03d56739daa0a167d2ace00d4
Mazire <mazirepooja666@gmail.com>
2024-12-10 19:27:20 +0530

    Define 'is_valid' function.

---
+++ sudoku_generator.py
@@ -0,0 +1,15 @@
+import random
+
+def is_valid(board, row, col, num):
+    for i in range(9):
+        if board[row][i] == num:
+            return False
+    for i in range(9):
+        if board[i][col] == num:
+            return False
+    start_row, start_col = 3 * (row // 3 ), 3 * (col // 3)
+    for i in range(start_row, start_row + 3):
+        for j in range(start_col, start_col + 3):
+            if board[i][j] == num:
+                return False
+    return True
